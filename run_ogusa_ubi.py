d3e4dfc3092db1ea27095785c2aac352629bcceb
Max Ghenis <mghenis@gmail.com>
2020-08-07 21:33:40 -0700

    Initial commit after running UBI and payroll tax small open economy experiments

--- run_ogusa_ubi.py
+++ run_ogusa_ubi.py
@@ -24,56 +24,43 @@ def main():
     print('Number of workers = ', num_workers)
     run_start_time = time.time()
 
-    # Grab a reform JSON file already in Tax-Calculator
-    # In this example the 'reform' is a change to 2017 law (the
-    # baseline policy is tax law in 2018)
-    reform_url = ('https://raw.githubusercontent.com/'
-                  'PSLmodels/Tax-Calculator/' + taxcalc.__version__ +
-                  '/taxcalc/reforms/2017_law.json')
-    ref = Calculator.read_json_param_objects(reform_url, None)
-    iit_reform = ref['policy']
+    # Create UBI reform
+    IIT_REFORM = {
+        'UBI_u18': {2021: 1000},
+        'UBI_1820': {2021: 1000},
+        'UBI_21': {2021: 1000}
+    }
 
     # Directories to save data
     CUR_DIR = os.path.dirname(os.path.realpath(__file__))
     base_dir = os.path.join(CUR_DIR, BASELINE_DIR)
     reform_dir = os.path.join(CUR_DIR, REFORM_DIR)
 
-    # Set some OG model parameters
-    # See default_parameters.json for more description of these parameters
-    alpha_T = np.zeros(50)  # Adjusting the path of transfer spending
-    alpha_T[0:2] = 0.09
-    alpha_T[2:10] = 0.09 + 0.01
-    alpha_T[10:40] = 0.09 - 0.01
-    alpha_T[40:] = 0.09
-    alpha_G = np.zeros(7)  # Adjusting the path of non-transfer spending
-    alpha_G[0:3] = 0.05 - 0.01
-    alpha_G[3:6] = 0.05 - 0.005
-    alpha_G[6:] = 0.05
-    # Set start year for baseline and reform.
-    START_YEAR = 2021
-    # Also adjust the Frisch elasticity, the start year, the
-    # effective corporate income tax rate, and the SS debt-to-GDP ratio
-    og_spec = {'frisch': 0.41, 'start_year': START_YEAR, 'cit_rate': [0.21],
-               'debt_ratio_ss': 1.0, 'alpha_T': alpha_T.tolist(),
-               'alpha_G': alpha_G.tolist()}
+    # Set OG model parameters for a small open economy.
+    # This is the same in baseline and reform.
+    OG_SPEC = {'start_year': 2021,
+               'zeta_D': [0],
+               'zeta_K': [1],
+               'initial_foreign_debt_ratio': 0
+               }
 
     '''
     ------------------------------------------------------------------------
     Run baseline policy first
     ------------------------------------------------------------------------
     '''
-    tax_func_path = os.path.join(
-        CUR_DIR, '..', 'ogusa', 'data', 'tax_functions',
-        'TxFuncEst_baseline_CPS.pkl')  # use cached baseline estimates
+    # tax_func_path = os.path.join(
+    #     CUR_DIR, '..', 'ogusa', 'data', 'tax_functions',
+    #     'TxFuncEst_baseline_CPS.pkl')  # use cached baseline estimates
     kwargs = {'output_base': base_dir, 'baseline_dir': base_dir,
-              'test': False, 'time_path': True, 'baseline': True,
-              'og_spec': og_spec, 'guid': '_example',
-              'run_micro': False, 'tax_func_path': tax_func_path,
+              'test': False, 'time_path': False, 'baseline': True,
+              'og_spec': OG_SPEC, 'guid': '_example',
+              'run_micro': False, # 'tax_func_path': tax_func_path,
               'data': 'cps', 'client': client,
               'num_workers': num_workers}
 
     start_time = time.time()
-    runner(**kwargs)
+    # runner(**kwargs)
     print('run time = ', time.time()-start_time)
 
     '''
@@ -81,15 +68,10 @@ def main():
     Run reform policy
     ------------------------------------------------------------------------
     '''
-    # update the effective corporate income tax rate
-    og_spec = {'frisch': 0.41, 'start_year': START_YEAR,
-               'cit_rate': [0.35], 'debt_ratio_ss': 1.0,
-               'alpha_T': alpha_T.tolist(),
-               'alpha_G': alpha_G.tolist()}
     kwargs = {'output_base': reform_dir, 'baseline_dir': base_dir,
-              'test': False, 'time_path': True, 'baseline': False,
-              'og_spec': og_spec, 'guid': '_example',
-              'iit_reform': iit_reform, 'run_micro': True, 'data': 'cps',
+              'test': False, 'time_path': False, 'baseline': False,
+              'og_spec': OG_SPEC, 'guid': '_example',
+              'iit_reform': IIT_REFORM, 'run_micro': True, 'data': 'cps',
               'client': client, 'num_workers': num_workers}
 
     start_time = time.time()
